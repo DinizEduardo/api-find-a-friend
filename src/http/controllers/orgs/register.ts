41108cbcdf09e10a2b62d912cd6db622e07452e3
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-07-18 20:46:36 -0300

    feat: cria uma nova org controller

---
+++ src/http/controllers/orgs/register.ts
@@ -0,0 +1,41 @@
+import { PrismaOrgRepository } from '@/repositories/prisma/prisma-org-repository'
+import { RegisterOrgUseCase } from '@/use-cases/org/register'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function registerOrg(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const registerOrgSchema = z.object({
+    name: z.string(),
+    address: z.string(),
+    whatsapp: z.string().min(11).max(11),
+    password: z.string().min(6),
+  })
+
+  const { name, address, whatsapp, password } = registerOrgSchema.parse(
+    request.body,
+  )
+
+  const orgsRepository = new PrismaOrgRepository()
+  const authenticateUseCase = new RegisterOrgUseCase(orgsRepository)
+
+  const { org } = await authenticateUseCase.execute({
+    name,
+    address,
+    whatsapp,
+    password,
+  })
+
+  const token = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: org.id,
+      },
+    },
+  )
+
+  return reply.status(201).send({ token })
+}
--- src/repositories/prisma/prisma-org-repository.ts
+++ src/repositories/prisma/prisma-org-repository.ts
@@ -4,6 +4,16 @@ import { Prisma } from '@prisma/client'
 import { OrgsRepository } from '../org-repository'
 
 export class PrismaOrgRepository implements OrgsRepository {
+  async findById(id: string) {
+    const org = await prisma.oRG.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return org
+  }
+
   async create(data: Prisma.ORGCreateInput) {
     const org = await prisma.oRG.create({
       data,
